9bc1f06ac31572e4389e64a4f14445cee3603c9b
Silvio Ruiz <silviojrck@gmail.com>
2023-05-31 14:24:08 -0300

    Se agregan las validaciones para el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import { Form, FormControl, FormGroup, FormLabel } from 'react-bootstrap';
+import { Form, FormControl, FormGroup, FormLabel, Button } from 'react-bootstrap';
 import ListaCitas from './ListaCitas';
+import { useForm } from 'react-hook-form';
+import { validarFecha } from './validators.js';
+import { useState } from 'react';
 
 const Formulario = () => {
+
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [citas, setCitas] = useState([]);
+
+    const onSubmit = (data) => {
+        setCitas([...citas, data]);
+        console.log(citas);
+    }
+
     return (
         <>
             <div className='border rounded text-light mt-5'>
@@ -10,29 +21,65 @@ const Formulario = () => {
                     <h2>Llenar el formulario para crear una cita</h2>
                 </div>
                 <div className='p-5 bg-dark'>
-                    <Form>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
                         <div className='row mb-4'>
                             <FormLabel className='col-md-3'>Nombre de la mascota:</FormLabel>
-                            <input className='col-md-9' placeholder='nombre de mascota' type='text'></input>
+                            <input className='col-md-9' placeholder='nombre de mascota' type='text' {...register("nombreMascota", {
+                                required: true,
+                                maxLength: 20,
+                                minLength: 2,
+                                pattern: /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/u
+                            })}></input>
+                            {errors.nombreMascota?.type === "required" && <div className='text-bg-danger'>Debe ingresar el nombre de la mascota</div>}
+                            {errors.nombreMascota?.type === "maxLength" && <div>El nombre de la mascota no debe superar los 20 caracteres</div>}
+                            {errors.nombreMascota?.type === "minLength" && <div>El nombre de la mascota debe ser de mas de 2 caracteres</div>}
+                            {errors.nombreMascota?.type === "pattern" && <div>Debe ingresar un nombre válido</div>}
                         </div>
                         <div className='row mb-4'>
                             <FormLabel className='col-md-3'>Nombre del dueño:</FormLabel>
-                            <input className='col-md-9' placeholder='nombre de dueño' type='text'></input>
+                            <input className='col-md-9' placeholder='nombre de dueño' type='text' {...register("nombreDuenio", {
+                                required: true,
+                                maxLength: 20,
+                                minLength: 3,
+                                pattern: /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/u
+                            })}></input>
+                            {errors.nombreDuenio?.type === "required" && <div>Debe ingresar el nombre del dueño</div>}
+                            {errors.nombreDuenio?.type === "maxLength" && <div>El nombre del dueño no debe superar los 20 caracteres</div>}
+                            {errors.nombreDuenio?.type === "minLength" && <div>El nombre del dueño debe tener al menos 2 catacteres</div>}
+                            {errors.nombreDuenio?.type === "pattern" && <div>Debe ingresar un nombre válido</div>}
                         </div>
                         <div className='row mb-4 d-flex align-items-center justify-content-around'>
                             <FormGroup className='col-md-6 row'>
                                 <FormLabel className='col-md-6'>Fecha:</FormLabel>
-                                <input className='col-md-6' type='date'></input>
+                                <input className='col-md-6' type='date' {...register("fecha", {
+                                    required: true,
+                                    validate: validarFecha
+                                })}></input>
+                                {errors.fecha?.type === "required" && <div>Debe ingresar una fecha</div>}
+                                {errors.fecha?.type === "validate" && <div>La fecha no puede ser anterior a hoy y no debe pasar por dos meses la fecha actual</div>}
                             </FormGroup>
                             <FormGroup  className='col-md-6 row d-flex align-items-center justify-content-around'>
                                 <FormLabel className='col-md-6'>Hora:</FormLabel>
-                                <input className='col-md-6' placeholder='hh:mm' type='text'></input>
+                                <input className='col-md-6' placeholder='hh:mm' type='text' {...register("hora", {
+                                    required: true,
+                                    pattern: /^(?:[01]\d|2[0-3]):[0-5]\d$/
+                                })}></input>
+                                {errors.hora?.type === "required" && <div>Debe ingresar un horario</div>}
+                                {errors.hora?.type === "pattern" && <div>Debe ingresar un horario en hh:mm (entre 00 y 23h y 00 y 59min)</div>}
                             </FormGroup>
                         </div>
                         <div className='row mb-4'>
                             <FormLabel className='col-md-3'>Sintomas:</FormLabel>
-                            <input className='col-md-9' placeholder='describir sintomas' type='text'></input>
+                            <input className='col-md-9' placeholder='describir sintomas' type='text' {...register("sintomas", {
+                                required: true,
+                                maxLength: 100,
+                                min:10
+                            })}></input>
+                            {errors.sintomas?.type === "required" && <div>Debe ingresar los sintomas</div>}
+                            {errors.sintomas?.type === "maxLength" && <div>La cantidad de caracteres no puede ser mayor a 100</div>}
+                            {errors.sintomas?.type === "minLength" && <div>La cantidad de caracteres no puede ser menor a 10</div>}
                         </div>
+                        <div><Button type="submit">Enviar</Button></div>
                     </Form>
                 </div>
                 <ListaCitas></ListaCitas>
