077a20215c9f86fc45a2c498c8cd1e53f00cdd50
Silvio Ruiz <silviojrck@gmail.com>
2023-05-31 14:28:22 -0300

    Agrega estilos a los errores

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -30,10 +30,12 @@ const Formulario = () => {
                                 minLength: 2,
                                 pattern: /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/u
                             })}></input>
-                            {errors.nombreMascota?.type === "required" && <div className='text-bg-danger'>Debe ingresar el nombre de la mascota</div>}
-                            {errors.nombreMascota?.type === "maxLength" && <div>El nombre de la mascota no debe superar los 20 caracteres</div>}
-                            {errors.nombreMascota?.type === "minLength" && <div>El nombre de la mascota debe ser de mas de 2 caracteres</div>}
-                            {errors.nombreMascota?.type === "pattern" && <div>Debe ingresar un nombre válido</div>}
+                            <div className='error-container'>
+                                {errors.nombreMascota?.type === "required" && <div className='error-msg'>Debe ingresar el nombre de la mascota</div>}
+                                {errors.nombreMascota?.type === "maxLength" && <div className='error-msg'>El nombre de la mascota no debe superar los 20 caracteres</div>}
+                                {errors.nombreMascota?.type === "minLength" && <div className='error-msg'>El nombre de la mascota debe ser de mas de 2 caracteres</div>}
+                                {errors.nombreMascota?.type === "pattern" && <div className='error-msg'>Debe ingresar un nombre válido</div>}
+                            </div>
                         </div>
                         <div className='row mb-4'>
                             <FormLabel className='col-md-3'>Nombre del dueño:</FormLabel>
@@ -43,10 +45,12 @@ const Formulario = () => {
                                 minLength: 3,
                                 pattern: /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/u
                             })}></input>
-                            {errors.nombreDuenio?.type === "required" && <div>Debe ingresar el nombre del dueño</div>}
-                            {errors.nombreDuenio?.type === "maxLength" && <div>El nombre del dueño no debe superar los 20 caracteres</div>}
-                            {errors.nombreDuenio?.type === "minLength" && <div>El nombre del dueño debe tener al menos 2 catacteres</div>}
-                            {errors.nombreDuenio?.type === "pattern" && <div>Debe ingresar un nombre válido</div>}
+                            <div className='error-container'>
+                                {errors.nombreDuenio?.type === "required" && <div className='error-msg'>Debe ingresar el nombre del dueño</div>}
+                                {errors.nombreDuenio?.type === "maxLength" && <div className='error-msg'>El nombre del dueño no debe superar los 20 caracteres</div>}
+                                {errors.nombreDuenio?.type === "minLength" && <div className='error-msg'>El nombre del dueño debe tener al menos 2 catacteres</div>}
+                                {errors.nombreDuenio?.type === "pattern" && <div className='error-msg'>Debe ingresar un nombre válido</div>}
+                            </div>
                         </div>
                         <div className='row mb-4 d-flex align-items-center justify-content-around'>
                             <FormGroup className='col-md-6 row'>
@@ -55,8 +59,10 @@ const Formulario = () => {
                                     required: true,
                                     validate: validarFecha
                                 })}></input>
-                                {errors.fecha?.type === "required" && <div>Debe ingresar una fecha</div>}
-                                {errors.fecha?.type === "validate" && <div>La fecha no puede ser anterior a hoy y no debe pasar por dos meses la fecha actual</div>}
+                                <div className='error-container'>
+                                    {errors.fecha?.type === "required" && <div className='error-msg'>Debe ingresar una fecha</div>}
+                                    {errors.fecha?.type === "validate" && <div className='error-msg'>La fecha no puede ser anterior a hoy y no debe pasar por dos meses la fecha actual</div>}
+                                </div>
                             </FormGroup>
                             <FormGroup  className='col-md-6 row d-flex align-items-center justify-content-around'>
                                 <FormLabel className='col-md-6'>Hora:</FormLabel>
@@ -64,8 +70,10 @@ const Formulario = () => {
                                     required: true,
                                     pattern: /^(?:[01]\d|2[0-3]):[0-5]\d$/
                                 })}></input>
-                                {errors.hora?.type === "required" && <div>Debe ingresar un horario</div>}
-                                {errors.hora?.type === "pattern" && <div>Debe ingresar un horario en hh:mm (entre 00 y 23h y 00 y 59min)</div>}
+                                <div className='error-container'>
+                                    {errors.hora?.type === "required" && <div className='error-msg'>Debe ingresar un horario</div>}
+                                    {errors.hora?.type === "pattern" && <div className='error-msg'>Debe ingresar un horario en hh:mm (entre 00 y 23h y 00 y 59min)</div>}
+                                </div>
                             </FormGroup>
                         </div>
                         <div className='row mb-4'>
@@ -75,9 +83,11 @@ const Formulario = () => {
                                 maxLength: 100,
                                 min:10
                             })}></input>
-                            {errors.sintomas?.type === "required" && <div>Debe ingresar los sintomas</div>}
-                            {errors.sintomas?.type === "maxLength" && <div>La cantidad de caracteres no puede ser mayor a 100</div>}
-                            {errors.sintomas?.type === "minLength" && <div>La cantidad de caracteres no puede ser menor a 10</div>}
+                            <div className='error-container'>
+                                {errors.sintomas?.type === "required" && <div className='error-msg'>Debe ingresar los sintomas</div>}
+                                {errors.sintomas?.type === "maxLength" && <div className='error-msg'>La cantidad de caracteres no puede ser mayor a 100</div>}
+                                {errors.sintomas?.type === "minLength" && <div className='error-msg'>La cantidad de caracteres no puede ser menor a 10</div>}
+                            </div>
                         </div>
                         <div><Button type="submit">Enviar</Button></div>
                     </Form>
